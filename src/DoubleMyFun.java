56e741f5698321c5453baae60690fe38efb2f3b2
mcguiron <mcguiron@mail.uc.edu>
2024-09-17 13:31:44 -0400

    Initial commit

---
+++ src/DoubleMyFun.java
@@ -0,0 +1,12 @@
+public class DoubleMyFun
+{
+    public static void main(String[] args)
+    {
+        int fun = 23;
+        int doubleFun = 0;
+
+        doubleFun = fun * 2;
+
+        System.out.println("Double your fun " +fun + " is " + doubleFun);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        boolean done= true;
+        String name = "owen-chan";
+        int age = 18;
+        double salary = 0;
+
+        System.out.println("Hello " + name);
+        System.out.println("You said your age was " + age);
+        System.out.println(done);
+    }
+}
